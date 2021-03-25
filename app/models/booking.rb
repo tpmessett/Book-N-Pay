41042f92e4de777a32f2674e8eb0de0a66556c6a
thomas messett <thomas.messett@stampapp.me>
2021-03-25 23:52:39 +0000

    added outline routes and controllers

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,2 +1,7 @@
 class Booking < ApplicationRecord
+  belongs_to :customer
+  belongs_to :user
+  validates :price, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
 end
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :bookings
+  belongs_to :user
+  validates :name, presence: true
 end
