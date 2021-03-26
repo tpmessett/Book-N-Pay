835cab101bf3387f3e69e944164c7377681b4665
thomas messett <thomas.messett@stampapp.me>
2021-03-26 00:44:04 +0000

    added policy scopes

---
+++ app/policies/booking_policy.rb
@@ -0,0 +1,24 @@
+class BookingPolicy < ApplicationPolicy
+  def index?
+    true
+  end
+
+  class Scope
+    attr_reader :user, :scope
+
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      if user.admin? || user.moderator?
+        scope.all
+      else
+        scope.where(user: user)
+      end
+    end
+
+  end
+
+end
