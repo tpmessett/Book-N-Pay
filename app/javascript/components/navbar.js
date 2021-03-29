2d926a44a0c61666ff0bfa40d94b3763c19de231
thomas messett <thomas.messett@stampapp.me>
2021-03-30 00:05:35 +0100

    navbar JS edits

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= (0.5*window.innerHeight)) {
         navbar.classList.add('navbar-lewagon-grey');
       } else {
         navbar.classList.remove('navbar-lewagon-grey');
